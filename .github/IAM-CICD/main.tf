969ddb37a316195df432ffd73f8a3769c27018a6
qday12 <qday137@gmail.com>
2025-12-30 11:57:07 +0100

    changed  IAM-CICD/main.tf to support existing OIDC provider

--- .github/IAM-CICD/main.tf
+++ .github/IAM-CICD/main.tf
@@ -27,11 +27,9 @@ variable "project_name" {
   default = "cloud-cluster-provisioning-eks"
 }
 
-# GitHub OIDC Provider
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+# GitHub OIDC Provider (already exists in AWS account)
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
 }
 
 # Plan Role (Read-Only)
@@ -44,7 +42,7 @@ resource "aws_iam_role" "plan" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = data.aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
@@ -107,7 +105,7 @@ resource "aws_iam_role" "apply" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = data.aws_iam_openid_connect_provider.github.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
